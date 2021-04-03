b8e822d139e15c774b48aece542a78b967d34eef
PeterDauwe <bobo5290461@gmail.com>
2021-04-03 08:15:25 +0200

    up

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -19,13 +19,13 @@ airootfs_image_tool_options=('-comp' 'xz')
 #airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '22')
 #airootfs_image_tool_options=('-comp' 'zstd')
 file_permissions=(
-  ["/etc/gshadow"]="0:0:600"	
+  ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:600"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
   ["/usr/local/bin/arcolinux-all-cores"]="0:0:755"
-  ["/usr/local/bin/arcolinux-before"]="0:0:755"
-  ["/usr/local/bin/arcolinux-final"]="0:0:755"
+  ["/usr/local/bin/arcobobo-before"]="0:0:755"
+  ["/usr/local/bin/arcobobo-final"]="0:0:755"
 )
